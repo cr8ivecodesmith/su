ae1d924b11ac1431bb7ea1d75878bc28c0f5ba46
Matt Lebrun <lebrun.matt@gmail.com>
2014-08-19 19:41:39 +0800

    First article

---
+++ content/a-box-of-chocolates.md
@@ -0,0 +1,16 @@
+Title: a box of chocolates
+Slug: a-box-of-chocolates
+Date: 2014-08-19 19:14:41
+Tags: none
+Category: none
+Author: matt blog
+
+> My momma always said, "Life was like a box of chocolates. You never know what you're gonna get."
+> - Forrest Gump
+
+I'm a curious guy. I look a the world with an eye of wonder. When I'm interested in something -- may
+it be a game, a skill, a piece of technology, a practice, etc. -- I tend to learn as much as I can
+about it. Use or learn it to the way it should be or bring it to its full potential. Sometimes I
+even go a step further and use or apply it in unexpected ways.
+
+This space is dedicated to those endeavors that I may always remember them.
---
+++ settings.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+AUTHOR = 'matt lebrun'
+SITENAME = 'super user'
+TAGLINE = ('Programmer/Creative. Creating awesome at Save22 and Advocate of Python Philippines. '
+           'Sometimes works on secret projects.')
+USER_LOGO_URL = 'http://www.gravatar.com/avatar/0f0e8de906724bbb54093f1852e7522e.png?size=140x140'
+SITEURL = 'http://su.mattlebrun.com'
+DEFAULT_DATE_FORMAT = '%A, %B %d, %Y'
+SUMMARY_MAX_LENGTH = (50)
+
+GITHUB_URL = 'https://github.com/cr8ivecodesmith/su'
+DISQUS_SITENAME = 'cr8ivecodesmith'
+GOOGLE_ANALYTICS = 'UA-9679489-4'
+
+FEED_DOMAIN = SITEURL
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+
+SOCIAL = (('@cr8ivecodesmith', 'https://twitter.com/cr8ivecodesmith'),
+          ('code', 'https://github.com/cr8ivecodesmith'),
+          ('community', 'http://pycon.python.ph/'),
+          ('email', 'mailto:talk@mattlebrun.com'),)
+TWITTER_USERNAME = 'cr8ivecodesmith'
+
+THEME = '.pelican-svbhack'
+TIMEZONE = 'Asia/Manila'
+DEFAULT_PAGINATION = 5
+
+MARKUP = (('md', 'rst'))
+MD_EXTENSIONS = (['codehilite', 'extra'])
